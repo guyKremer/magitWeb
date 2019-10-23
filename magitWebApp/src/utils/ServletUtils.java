2a83a2b0060fcf98cdd25531eadd3c38e03f5ca7
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-10-23 17:59:49 +0300

    update: user and messages objects

--- magitWebApp/src/utils/ServletUtils.java
+++ magitWebApp/src/utils/ServletUtils.java
@@ -13,14 +13,14 @@ import java.io.PrintWriter;
 public class ServletUtils {
 
     private static final String USER_MANAGER_ATTRIBUTE_NAME = "userManager";
-    private static final String CHAT_MANAGER_ATTRIBUTE_NAME = "chatManager";
+    //private static final String CHAT_MANAGER_ATTRIBUTE_NAME = "chatManager";
 
     /*
     Note how the synchronization is done only on the question and\or creation of the relevant managers and once they exists -
     the actual fetch of them is remained un-synchronized for performance POV
      */
     private static final Object userManagerLock = new Object();
-    private static final Object chatManagerLock = new Object();
+    //private static final Object chatManagerLock = new Object();
 
     public static UserManager getUserManager(ServletContext servletContext) {
 
