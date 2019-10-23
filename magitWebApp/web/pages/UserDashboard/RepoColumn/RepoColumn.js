78bd3c978aedb27eb233e30cc2ab28fc7fa5d5d1
guyKremer <42372467+guyKremer@users.noreply.github.com>
2019-10-23 23:00:04 +0300

    formData fetch

--- magitWebApp/web/pages/UserDashboard/RepoColumn/RepoColumn.js
+++ magitWebApp/web/pages/UserDashboard/RepoColumn/RepoColumn.js
@@ -44,10 +44,13 @@ export default class RepoColumn extends React.Component{
             return(
                 <div id="userDash-left">
                     <div id="userDash-left-first">
-                        <input type="file" onChange={()=>{fetch(  "repositories?userName="+this.props.userName, {method:'POST', body: '',  headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'multipart/form-data'
-                            },credentials: 'include'});}}/>
+                        <input type="file" id="input" onChange={
+                            ()=>{
+                                let selectedFile = document.getElementById('input');
+                                let  formData = new FormData();
+                                formData.append("file",selectedFile);
+                                fetch("repositories?userName="+this.props.userName, {method:'POST', body: formData,credentials: 'include'});
+                            }}/>
                     </div>
                     <RepoList/>
                 </div>
