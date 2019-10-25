6ad8df223aa47f203614c7906f9964f55da6ba66
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-10-25 12:22:46 +0300

    update: repos of users

--- Engine/src/users/PRMessage.java
+++ Engine/src/users/PRMessage.java
@@ -8,15 +8,23 @@ public class PRMessage {
     }
 
     private String repositoryName;
-    private String userName;
+    private String targetUserName;
+    private String basisUserName;
     private String targetBranch;
     private String basisBranch;
     private String PRMsg;
     private PRMessage.Status status;
 
-    public PRMessage(String i_repoName, String i_userName, String i_targetBranch, String i_basisBranch, String i_PRMsg){
+    public PRMessage(
+            String i_repoName,
+            String i_targetUserName,
+            String i_basisUserName,
+            String i_targetBranch,
+            String i_basisBranch,
+            String i_PRMsg){
         repositoryName = i_repoName;
-        userName = i_userName;
+        targetUserName = i_targetUserName;
+        basisUserName = i_basisUserName;
         targetBranch = i_targetBranch;
         basisBranch = i_basisBranch;
         PRMsg = i_PRMsg;
