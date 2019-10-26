2f4460121e53ee78b7230c000e7f40c4c1cb345e
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-10-26 11:04:04 +0300

    update: userRepoServlet

---
+++ magitWebApp/src/servlets/userReposServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import Engine.MagitObjects.Repository;
+import com.google.gson.JsonArray;
+import com.sun.corba.se.impl.orbutil.RepositoryIdStrings;
+import users.UserManager;
+import utils.ServletUtils;
+import utils.SessionUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static constants.Constants.*;
+
+@WebServlet(name = "userReposServlet", urlPatterns = {"/userRepos"})
+public class userReposServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userNameFromParameter = SessionUtils.getUsername(request);
+        String branchName = request.getParameter(USERNAME);
+
+        UserManager userManager = ServletUtils.getUserManager(getServletContext());
+        JsonArray jsonArray = new JsonArray();
+
+        for(Repository repo : userManager.getRepositories(userNameFromParameter)){
+            jsonArray.add(repo.GetName());
+        }
+
+
+    }
+}
