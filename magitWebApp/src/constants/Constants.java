1ea0822567e4e13b7206e4556dc39ec0ad83ae62
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-10-31 15:04:49 +0200

    update: PRSERVLET

--- magitWebApp/src/constants/Constants.java
+++ magitWebApp/src/constants/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
     public static final String USERNAME= "userName";
     public static final String REPOSITORY = "repository";
     public static final String BRANCH = "branch";
+    public static final String BASISBRANCH = "BasisBranch";
+    public static final String TARGETBRANCH = "targetBranch";
+    public static final String PRMSG = "prMsg";
     public static final String FOLDERITEM = "folderItem";
     public static final String OPERATION = "operation";
     public static final String REMOTEREPO = "remoteRepo";
@@ -13,7 +16,5 @@ public class Constants {
     public static final String ISRTB ="isRTB";
     public static final String COMMITMSG = "commitMsg";
     public static final String ONLYNAMES= "onlyNames";
-
-
 
 }
--- magitWebApp/src/servlets/PRServlet.java
+++ magitWebApp/src/servlets/PRServlet.java
@@ -1,13 +1,28 @@
 package servlets;
 
+import Engine.MagitObjects.PRRequest;
+import users.UserManager;
+import utils.ServletUtils;
+import utils.SessionUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static constants.Constants.*;
+
 public class PRServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserManager userManager = ServletUtils.getUserManager(getServletContext());
+        String userNameFromParameter = SessionUtils.getUsername(request);
+        String remoteRepo = request.getParameter(REPOSITORY);
+        String remoteUser = request.getParameter(REMOTEUSER);
+        String basisBranch = request.getParameter(BASISBRANCH);
+        String targetBranch = request.getParameter(TARGETBRANCH);
+
+        new PRRequest()
 
     }
 }
